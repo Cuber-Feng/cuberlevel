209cec7b9b84f2722f08c2ad8f538fcc7d634414
Maple Feng <maple.feng@senmedia.com.hk>
2025-07-09 17:41:56 +0800

    finish the score calculate

---
+++ js/score.js
@@ -0,0 +1,39 @@
+class Event {
+    eventId;
+    wr = 0;
+    top01 = 0;
+    top1 = 0;
+    top5 = 0;
+    top10 = 0;
+    top20 = 0;
+    top50 = 0;
+    slowest = 0;
+    count = 0;
+
+    constructor(eventId) {
+        this.eventId = eventId;
+    }
+
+    getGrade(result = -1) {
+        if (result == this.wr)
+            return 100;
+        else if (result <= this.top01)
+            return 95 + (this.top01 - result) / (this.top01 - this.wr) * 5;
+        else if (result <= this.top1)
+            return 90 + (this.top1 - result) / (this.top1 - this.top01) * 5;
+        else if (result <= this.top5)
+            return 80 + (this.top5 - result) / (this.top5 - this.top1) * 10;
+        else if (result <= this.top10)
+            return 70 + (this.top10 - result) / (this.top10 - this.top5) * 10;
+        else if (result <= this.top20)
+            return 60 + (this.top20 - result) / (this.top20 - this.top10) * 10;
+        else if (result <= this.top50)
+            return 50 + (this.top50 - result) / (this.top50 - this.top20) * 10;
+        else if (result > -1)
+            return 40 + (this.slowest - result) / (this.slowest - this.top50) * 10;
+        else
+            return 0;
+    }
+}
+
+const eventDict = new Map();
--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ document.getElementById('lookupBtn').addEventListener('click', () => {
             document.getElementById('info-other').innerHTML = `
             <div>
             <span><strong>WCA ID:</strong> ${person.wca_id}</span>
-            <span><strong>Country:</strong> ${person.country?person.country.name : countryCodeMap[person.country_iso2]}</span>
+            <span><strong>Country:</strong> ${person.country ? person.country.name : countryCodeMap[person.country_iso2]}</span>
             </div>
             <div>
             <span><strong>Gender:</strong> ${gender}</span>
@@ -38,13 +38,13 @@ document.getElementById('lookupBtn').addEventListener('click', () => {
             thead.innerHTML = `
             <tr>
             <th class='sticky-col'>Event</th><th>NR</th><th>CR</th><th>WR</th>
-            <th>Single</th><th>Average</th><th>WR</th><th>CR</th><th>NR</th>
+            <th>Single</th><th class='score'>Score</th><th>Average</th><th>WR</th><th>CR</th><th>NR</th>
             </tr>
             `
             table.appendChild(thead);
 
             let tbody = document.createElement('tbody');
-
+            let sumScore = 0;
             Object.entries(data.personal_records).forEach(([eventId, record]) => {
                 if (!eventList.includes(eventId))
                     return;
@@ -72,6 +72,10 @@ document.getElementById('lookupBtn').addEventListener('click', () => {
                     }
 
                     if (record.average) {
+                        let sc = createEl('td', eventDict.get(eventId).getGrade(record.average.best).toFixed(2));
+                        sc.classList = 'score';
+                        tr.appendChild(sc);
+                        sumScore = sumScore + eventDict.get(eventId).getGrade(record.average.best);
                         if (eventId == '333fm') {
                             tr.appendChild(createEl('td', record.average.best));
                         } else if (eventId == '333mbf' || eventId == '333mbo') {
@@ -92,6 +96,13 @@ document.getElementById('lookupBtn').addEventListener('click', () => {
 
                 tbody.appendChild(tr);
             });
+
+            let overall = (sumScore / 16).toFixed(2);
+            document.getElementById('overallScore').classList.remove('hidden');
+            document.getElementById('overallScore').innerHTML = `
+            <strong>Overall Score: </strong>
+            <span>${overall}</span>
+            `;
 
             table.appendChild(tbody);
             document.getElementById('gradeTable').innerHTML = ``;
@@ -121,15 +132,22 @@ fetch('./event_rank_summary.csv')
         rows.forEach((row, rowIndex) => {
             const tr = document.createElement('tr');
             const cells = row.split(',');
+            let cur_eventId;
+            let event_obj;
+
             cells.forEach((cell, colIndex) => {
                 const cellEl = document.createElement(rowIndex == 0 || colIndex == 0 ? 'th' : 'td');
                 if (eventList.includes(cell)) {
+                    cur_eventId = cell;
                     let img = document.createElement('img');
                     img.src = `./cube_icons/${cell}.svg`;
                     img.style.width = "1.5rem";
                     cellEl.appendChild(img);
                     cellEl.id = 'event-cell';
                     cellEl.classList = 'sticky-col';
+
+                    event_obj = new Event(cell);
+
                 } else if (cell == 'eventId') {
                     cellEl.textContent = 'Event';
                     cellEl.classList = 'sticky-col';
@@ -146,11 +164,52 @@ fetch('./event_rank_summary.csv')
                 } else {
                     cellEl.textContent = formatResult(cell);
                 }
+
+                if (event_obj) {
+                    switch (colIndex) {
+                        case 1:
+                            event_obj.wr = cell;
+                            break;
+                        case 2:
+                            event_obj.top01 = cell;
+                            break;
+                        case 3:
+                            event_obj.top1 = cell;
+                            break;
+                        case 4:
+                            event_obj.top5 = cell;
+                            break;
+                        case 5:
+                            event_obj.top10 = cell;
+                            break;
+                        case 6:
+                            event_obj.top20 = cell;
+                            break;
+                        case 7:
+                            event_obj.top50 = cell;
+                            break;
+                        case 8:
+                            event_obj.slowest = cell;
+                            break;
+                        case 9:
+                            event_obj.count = cell.trim();
+                            break;
+                        default:
+                    }
+                    if (colIndex == cells.length - 1) {
+                        eventDict.set(cur_eventId, event_obj);
+                        // console.log(event_obj);
+                        event_obj = null;
+                    }
+                }
+
                 tr.appendChild(cellEl);
             });
 
             table.appendChild(tr);
         });
+
+        console.log(eventDict);
 
         const container = document.getElementById('table-container');
         container.innerHTML = '';
