a114b2d215db5764f73360c0dfc321e7649427a0
Maple Feng <maple.feng@senmedia.com.hk>
2025-07-17 08:58:10 +0800

    add mode toggle button

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,5 +1,7 @@
 const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
 const body = document.body;
+const topButton = document.getElementById('topimg');
+const sideButton = document.getElementById('sideimg');
 
 mediaQuery.addEventListener('change', (e) => {
     const isDark = e.matches;
@@ -13,17 +15,28 @@ mediaQuery.addEventListener('change', (e) => {
 function goDark() {
     if (!body.classList.contains('dark')) {
         body.classList.add('dark');
+        topButton.src = './web_icons/moon.png';
+        sideButton.src = './web_icons/moon.png';
     }
 }
 
 function goLight() {
     if (body.classList.contains('dark')) {
         body.classList.remove('dark');
+        topButton.src = './web_icons/sun.png';
+        sideButton.src = './web_icons/sun.png';
     }
 }
 
 function toggleMode() {
     body.classList.toggle('dark');
+    if (body.classList.contains('dark')) {
+        topButton.src = './web_icons/moon.png';
+        sideButton.src = './web_icons/moon.png';
+    } else {
+        topButton.src = './web_icons/sun.png';
+        sideButton.src = './web_icons/sun.png';
+    }
 }
 
 // 页面加载时
